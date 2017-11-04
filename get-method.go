7c4ba0bb49a8cfbf8f9a80a8eb185ff662832b3a
numb86 <86numb@gmail.com>
2017-11-04 11:37:36 +0900

    70 Getでステータスコードやヘッダーなどを取得する。

--- get-method.go
+++ get-method.go
@@ -4,7 +4,10 @@ import (
   "io/ioutil"
   "fmt"
   "net/http"
+  "reflect"
 )
+
+
 
 func main() {
   resp, err := http.Get("http://localhost:18888")
@@ -16,5 +19,12 @@ func main() {
   if err != nil {
     panic(err)
   }
-  fmt.Println("レスポンスボディ->", string(body))
+  fmt.Println("レスポンスボディ:", string(body))
+  fmt.Println("Status:", resp.Status)
+  fmt.Println("StatusCode", resp.StatusCode)
+  fmt.Println("Headers", resp.Header)
+  fmt.Println("Content_Length", resp.Header.Get("Content-Length"))
+  result := resp.Header.Get("Hoge")
+  fmt.Println(reflect.TypeOf(result)) // string
+  fmt.Println(result == "") // true
 }
