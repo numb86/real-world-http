1d11f521026f19342ea101e0751f816b191481e5
numb86 <86numb@gmail.com>
2017-11-13 20:55:16 +0900

    79 Cookieの送受信。

---
+++ cookie.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+  "fmt"
+  "net/http"
+  "net/http/cookiejar"
+  "net/http/httputil"
+)
+
+func main() {
+  jar, err := cookiejar.New(nil)
+  if err != nil {
+    panic(err)
+  }
+  client := http.Client{
+    Jar: jar,
+  }
+  for i := 0; i < 2; i++ {
+    resp, err := client.Get("http://localhost:18888/cookie")
+    if err != nil {
+      panic(err)
+    }
+    dump, err := httputil.DumpResponse(resp, true)
+    if err != nil {
+      panic(err)
+    }
+    fmt.Println(string(dump))
+  }
+}
